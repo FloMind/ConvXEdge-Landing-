21bd903bbc16cdaf6a7d8d4546e91b31c3d57f1a
FloMind <florent.cochet7@gmail.com>
2025-02-22 16:53:48 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let ctaButton = document.querySelector(".cta-btn");
+    ctaButton.addEventListener("click", function() {
+        alert("🚀 Inscription à la Bêta bientôt disponible !");
+    });
+});
